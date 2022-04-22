e2322cf06b2a6a3ec81079ae4c796e69105c5888
Santhosh <89012828+santhoshsanthoshss@users.noreply.github.com>
2022-04-23 00:08:12 +0530

    Add files via upload
    The project is based on the calculator ..

---
+++ script.js
@@ -0,0 +1,26 @@
+let outputscreen =document.getElementById("output-screen");
+
+ function display(num){
+    outputscreen.value += num;
+ }
+
+function calculate(){
+    try{
+        outputscreen.value = eval(outputscreen.value);
+    }
+
+    catch(err)
+    {
+        alert("invalid");
+    }
+
+}
+
+function clearScreen(){
+    outputscreen.value ="0";
+    outputscreen.textContent ="0";
+}
+
+function del(){
+    outputscreen.value = outputscreen.value .slice(0,-1);
+}
